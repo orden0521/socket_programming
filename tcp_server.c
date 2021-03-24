c32582ceda91bdbab127d9128e073fb2cac30b35
orden0521 <audrb3170@gmail.com>
2021-03-24 21:09:48 +0900

    tcp_server.c upload

--- tcp_server.c
+++ tcp_server.c
@@ -8,8 +8,7 @@
 
 void error_handling(char *message);
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
 	int serv_sock;
 	int clnt_sock;
 
@@ -17,9 +16,9 @@ int main(int argc, char *argv[])
 	struct sockaddr_in clnt_addr;
 	socklen_t clnt_addr_size;
 
-	char message[]="Hello World!";
+	char message[] = "Hello World!";
 	
-	if(argc!=2){
+	if(argc != 2) {
 		printf("Usage : %s <port>\n", argv[0]);
 		exit(1);
 	}
@@ -29,19 +28,19 @@ int main(int argc, char *argv[])
 		error_handling("socket() error");
 	
 	memset(&serv_addr, 0, sizeof(serv_addr));
-	serv_addr.sin_family=AF_INET;
-	serv_addr.sin_addr.s_addr=htonl(INADDR_ANY);
-	serv_addr.sin_port=htons(atoi(argv[1]));
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+	serv_addr.sin_port = htons(atoi(argv[1]));
 	
-	if(bind(serv_sock, (struct sockaddr*) &serv_addr, sizeof(serv_addr))==-1)
+	if(bind(serv_sock, (struct sockaddr*) &serv_addr, sizeof(serv_addr)) == -1)
 		error_handling("bind() error"); 
 	
-	if(listen(serv_sock, 5)==-1)
+	if(listen(serv_sock, 5) == -1)
 		error_handling("listen() error");
 	
-	clnt_addr_size=sizeof(clnt_addr);  
-	clnt_sock=accept(serv_sock, (struct sockaddr*)&clnt_addr,&clnt_addr_size);
-	if(clnt_sock==-1)
+	clnt_addr_size = sizeof(clnt_addr);  
+	clnt_sock = accept(serv_sock, (struct sockaddr*)&clnt_addr,&clnt_addr_size);
+	if(clnt_sock == -1)
 		error_handling("accept() error");  
 	
 	write(clnt_sock, message, sizeof(message));
@@ -50,8 +49,7 @@ int main(int argc, char *argv[])
 	return 0;
 }
 
-void error_handling(char *message)
-{
+void error_handling(char *message) {
 	fputs(message, stderr);
 	fputc('\n', stderr);
 	exit(1);
