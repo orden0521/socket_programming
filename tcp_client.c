f8c63fd2ae05c84daf944e22398355be11007cdf
orden0521 <audrb3170@gmail.com>
2021-03-24 21:08:33 +0900

    tcp_client.c upload

--- tcp_client.c
+++ tcp_client.c
@@ -13,32 +13,32 @@ int main(int argc, char* argv[])
 	int sock;
 	struct sockaddr_in serv_addr;
 	char message[30];
-	int str_len=0;
-	int idx=0, read_len=0;
+	int str_len = 0;
+	int idx = 0, read_len = 0;
 	
-	if(argc!=3){
+	if(argc != 3) {
 		printf("Usage : %s <IP> <port>\n", argv[0]);
 		exit(1);
 	}
 	
-	sock=socket(PF_INET, SOCK_STREAM, 0);
+	sock = socket(PF_INET, SOCK_STREAM, 0);
 	if(sock == -1)
 		error_handling("socket() error");
 	
 	memset(&serv_addr, 0, sizeof(serv_addr));
-	serv_addr.sin_family=AF_INET;
-	serv_addr.sin_addr.s_addr=inet_addr(argv[1]);
-	serv_addr.sin_port=htons(atoi(argv[2]));
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_addr.s_addr = inet_addr(argv[1]);
+	serv_addr.sin_port = htons(atoi(argv[2]));
 		
-	if(connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr))==-1) 
+	if(connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) == -1) 
 		error_handling("connect() error!");
 
-	while(read_len=read(sock, &message[idx++], 1))
+	while(read_len = read(sock, &message[idx++], 1))
 	{
-		if(read_len==-1)
+		if(read_len == -1)
 			error_handling("read() error!");
 		
-		str_len+=read_len;
+		str_len += read_len;
 	}
 
 	printf("Message from server: %s \n", message);
@@ -47,8 +47,7 @@ int main(int argc, char* argv[])
 	return 0;
 }
 
-void error_handling(char *message)
-{
+void error_handling(char *message) {
 	fputs(message, stderr);
 	fputc('\n', stderr);
 	exit(1);
